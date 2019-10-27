0ea98014a0892a10b52e524a289087ece53f87f1
Nilson <mgtalsc@gmail.com>
2019-10-27 18:23:48 -0400

    New Proyect Spring

---
+++ src/main/java/com/odont/odont/OdontApplication.java
@@ -0,0 +1,13 @@
+package com.odont.odont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odont/odont/OdontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odont.odont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
