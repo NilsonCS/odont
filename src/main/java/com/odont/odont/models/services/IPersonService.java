957b99bafda5dd60f072266675e0b1929f6b825a
Nilson <mgtalsc@gmail.com>
2019-10-29 13:16:56 -0400

    Implementando metodos CRUD para person

--- src/main/java/com/odont/odont/models/services/IPersonService.java
+++ src/main/java/com/odont/odont/models/services/IPersonService.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface IPersonService {
 
     public List<PersonEntity> findAll();
+
+    public PersonEntity findById(Long person_id);
+
+    public PersonEntity save (PersonEntity personEntity);
+
+    public void delete(Long id);
 }
--- src/main/java/com/odont/odont/models/services/PersonServiceImpl.java
+++ src/main/java/com/odont/odont/models/services/PersonServiceImpl.java
@@ -13,9 +13,30 @@ public class PersonServiceImpl implements IPersonService {
 
     @Autowired
     private IPersonDao personDao;
+
     @Override
     @Transactional(readOnly = true)
     public List<PersonEntity> findAll() {
         return (List<PersonEntity>) personDao.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+
+    public PersonEntity findById(Long person_id) {
+        return personDao.findById(person_id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+
+    public PersonEntity save(PersonEntity personEntity) {
+        return personDao.save(personEntity);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        personDao.deleteById(id);
     }
 }
