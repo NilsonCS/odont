8ffa9510853f6f06da2ee478d84b5c1ae6642e6c
Nilson <mgtalsc@gmail.com>
2019-11-22 13:57:30 -0400

    Correcciones, funcionando sin error

--- src/main/java/com/odont/odont/handler/TelegramMessageHandler.java
+++
@@ -1,7 +0,0 @@
-package com.odont.odont.handler;
-
-import com.odont.odont.models.telegram.TelegramUpdate;
-
-public interface TelegramMessageHandler {
-    void handle(TelegramUpdate telegramUpdate);
-}
